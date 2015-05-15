eab744fee0a675158c9a83cd5625a7cc4f087aef
Shawn Mehan <shanw.mehan@ieee.org>
2015-05-15 10:41:48 -0700

    Added histogram with a facet_grid, hist(Num of Subtasks)~year_created~Assignee

--- plot.r
+++ plot.r
@@ -68,4 +68,22 @@ ggplot(myData) +
     labs(x="Project Creation Month", y="Number of subtasks") +
     theme(axis.text.x = element_text(angle = 90)) +
     theme(axis.title.y = element_text(vjust=0.5)) +
-    theme(axis.title.x = element_text(vjust=-0.1))
+    theme(axis.title.x = element_text(vjust=-0.1)) +
+    theme(plot.title = element_text(size=20, face="bold", vjust=2))
+
+# change the level attributes for plot purposes
+# this is kind of clunky but I haven't found a better way to do this on the fly.
+levels(x=myData$Assignee)<-c("DW", "ER", "MR", "TMAY", "NONE")
+ggplot(myData) +
+    aes(myData$X..of.Sub.Tasks) +
+    geom_histogram(fill="firebrick") +
+    facet_grid(Assignee~year_created) +
+    theme_stata() +
+    ggtitle("Subtasks per assignee per year created") +
+    labs(x="Subtasks", y="Frequency") +
+    theme(axis.text.x = element_text(angle = 90)) +
+    theme(axis.title.y = element_text(vjust=0.5)) +
+    theme(axis.title.x = element_text(vjust=-0.1)) +
+    theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
+    theme(strip.text.y = element_text(colour = "red", angle = 45, size = 10,
+                                      hjust = 0.5, vjust = 0.5))
