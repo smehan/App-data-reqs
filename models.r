3530f235880150b9ff26b3d8303ca4ffcd03cdca
Shawn Mehan <shanw.mehan@ieee.org>
2015-05-24 16:01:17 -0700

    Added a class for linear models

---
+++ models.r
@@ -0,0 +1,6 @@
+###########################################################
+### Models for durations
+###########################################################
+
+# First read in data set
+readRDS(file="data/App_Data_Reqs.rds")
