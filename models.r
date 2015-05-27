d0884d5679e3ee5d89a9487ba67753404d77a227
Shawn Mehan <shanw.mehan@ieee.org>
2015-05-26 23:13:18 -0700

    scatter plot month_created~project duration

--- models.r
+++ models.r
@@ -4,3 +4,9 @@
 
 # First read in data set
 readRDS(file="data/App_Data_Reqs.rds")
+
+ggplot(myData) +
+    aes(x=month_num_created, y=as.numeric(project_duration)) +
+    geom_point(aes(color=factor(Assignee))) +
+    facet_wrap(~year_created) +
+    ggtitle("Project duration by \nmonth project created by year created")
