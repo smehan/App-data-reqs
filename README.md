b71ea251420b5083a0270c6320c7c0ede9b7de06
Shawn Mehan <shanw.mehan@ieee.org>
2015-07-09 02:39:22 -0700

    Created pre-processor script to read in and clean up csv.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # App-data-reqs
 R to analyze cp app data requests
+
+The data is being pulled from JIRA and now has multiple empty columns
+due to lack of data quality control on entry.
---
+++ log-preprocessor.py
@@ -0,0 +1,29 @@
+__author__ = 'shawnmehan'
+
+import csv, os, re
+
+
+# # first lets open the file, which is tab delimited because of , in description field and others
+with open('./data/AppDataRequest2010-2015.tsv', 'rb') as csvfile:
+    testfile = open("./data/test.tsv", "wb") #TODO get proper line endings, not ^M
+    records = csv.reader(csvfile, delimiter='\t')
+    testwriter = csv.writer(testfile, delimiter='\t')
+    count = 0
+    for row in records:
+        for e in row: #TODO still need to remove \n from list elements
+            e = re.sub(r'\n','',e)
+        testwriter.writerow(row)
+        count += 1
+    # for row in records:
+    #     target = ','.join(row)
+    #     print(target)
+    #     if re.match(".*(?<!EOR)$", target):
+    #         testwriter.writerow(row)
+    #     elif re.match(".*EOR$", target):
+    #         # print(row)
+    #         # count += 1
+    #         testwriter.writerow(row)
+    #     else:
+    #         testwriter.writerow(target)
+    print count
+    testfile.close()
