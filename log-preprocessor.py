6ead816506341cb1a953f08dfe08c580e8b74148
Shawn Mehan <shanw.mehan@ieee.org>
2015-07-09 10:13:38 -0700

    got new lines out, cleaned up counts and reporting

--- log-preprocessor.py
+++ log-preprocessor.py
@@ -1,29 +1,17 @@
 __author__ = 'shawnmehan'
 
-import csv, os, re
+import csv, re
 
-
-# # first lets open the file, which is tab delimited because of , in description field and others
+# first lets open the file, which is tab delimited because of , in description field and others
 with open('./data/AppDataRequest2010-2015.tsv', 'rb') as csvfile:
-    testfile = open("./data/test.tsv", "wb") #TODO get proper line endings, not ^M
+    outfile = open('./data/AppDataRequest2010-2015-clean.tsv', 'wb')  # TODO get proper line endings, not ^M
     records = csv.reader(csvfile, delimiter='\t')
-    testwriter = csv.writer(testfile, delimiter='\t')
+    outwriter = csv.writer(outfile, delimiter='\t')
     count = 0
-    for row in records:
-        for i, s in enumerate(row):
+    for row in records:  # iterate through each row to clear out errant newlines
+        for i, s in enumerate(row):  # i is list index, s is string
             row[i] = re.sub(r'\n+', '', s)
-        testwriter.writerow(row)
+        outwriter.writerow(row)  # write the clean row
         count += 1
-    # for row in records:
-    #     target = ','.join(row)
-    #     print(target)
-    #     if re.match(".*(?<!EOR)$", target):
-    #         testwriter.writerow(row)
-    #     elif re.match(".*EOR$", target):
-    #         # print(row)
-    #         # count += 1
-    #         testwriter.writerow(row)
-    #     else:
-    #         testwriter.writerow(target)
-    print count
-    testfile.close()
+    print u"There were {0:d} lines processed in this run.".format(count)
+    outfile.close()
