cd738e5c988c7564cbb8304fda45f7b89185b795
Shawn Mehan <shanw.mehan@ieee.org>
2015-07-30 17:13:50 -0700

    Have melt working with myData, correctly assigning column vectors in melt.

--- Approver.R
+++ Approver.R
@@ -8,14 +8,29 @@
 myData <- readRDS(file="data/App_Data_Reqs.rds")
 
 # some scratch
-trial <- data.frame(id = factor(1:4), 
-                    A1 = c(1, 2, 1, 2), 
-                    A2 = c(2, 1, 2, 1),
-                    B1 = c(3, 3, 3, 3))
-trialm <- melt(trial)
+# trial <- data.frame(id = factor(1:4), 
+#                     A1 = c(1, 2, 1, 2), 
+#                     A2 = c(2, 1, 2, 1),
+#                     B1 = c(3, 3, 3, 3))
+# trialm <- melt(trial)
 
-df <- melt(myData, id.vars = c("Key"),
-           measured.vars = c("Assignee"),
-           na.rm = TRUE,
-           factorsAsStrings = F)
+# if we need to identify and remove Key dups
+dups <- anyDuplicated(approverDF$Key)
+approverDF <- approverDF[-dups, ]
+
+# Create the approver DF as a subset of myData
+approverDF <- data.frame(Key = factor(myData$Key),
+                 Project = myData$Project,
+                 Creator = myData$Creator,
+                 Assignee = myData$Assignee)
+
+# convert to long form
+approverDF <- melt(approverDF, 
+                   id.vars = c("Key"),
+                   measured.vars = c("Project, Creator, Assignee"),
+                   na.rm = TRUE,
+                   variable.name = "stuff",
+                   value.name = "values",
+                   factorsAsStrings = T)
+
 
