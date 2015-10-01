05e06d1658020be1cf4299064b4c110e331de0a2
Theresa May <theresa.may.001@gmail.com>
2015-09-30 23:27:32 -0700

    Added class to manipulate data from Approver.rds and create a pareto distribution and plot.

---
+++ Pareto.R
@@ -0,0 +1,41 @@
+###########################################################
+### Creates a paretoDFa frame of counts for Duration.AT from
+### approver.R paretoDFa and makes a pareto plot of durations
+### and times.
+###########################################################
+
+library(ggplot2)
+
+# First read in paretoDFa set
+paretoDF <- readRDS(file="paretoDFa/App_paretoDFa_Reqs.rds")
+
+# Subset only durations
+paretoDF <- paretoDF[c("Key", "Duration.AT")]
+paretoDF <- count(paretoDF, Duration.AT, sort=TRUE)
+
+# Change colnames to more useful lables
+colnames(paretoDF)[2] <- "Count"
+
+# pareto.chart(approverDF$Duration.AT, ylab="Something")
+
+# reorder on decreasing count
+paretoDF <- paretoDF[order(paretoDF$Count, decreasing=TRUE), ]
+
+# rewrite Duration with factors and levels
+paretoDF$Duration.AT <- factor(paretoDF$Duration.AT, levels=paretoDF$Duration.AT)
+
+# Create cumsums of counts in new column
+paretoDF$cum <- cumsum(paretoDF$Count)
+
+# output the DF
+paretoDF
+
+###########################################################
+### Build some plots
+###########################################################
+ggplot(paretoDF, aes(x=Duration.AT)) +
+    geom_bar(aes(y=Count), fill="blue", stat="identity") +
+    geom_point(aes(y=cum)) +
+    geom_path(aes(y=cum, group=1)) +
+    ggtitle("Pareto Chart of Approval Cycle Times") +
+    labs(x="Duration (days)", y="Counts")    
