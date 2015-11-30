099397582775b5c70a66505805cf1efb6fe84672
theresamay <theresa.may.001@gmail.com>
2015-11-29 20:25:58 -0800

    Removing first heatmap without Questions var, and using only one heatmap in total.

--- Correlations.R
+++ Correlations.R
@@ -9,6 +9,7 @@
 library(dplyr)
 library(ggplot2)
 library(reshape2)
+library(scales)
 
 # First read in data set
 approvalsDF <- readRDS(file="data/Approvals.rds")
@@ -51,31 +52,14 @@ approvalsQuestCoded <- approvalsQuestCoded[c(-33)]
 
 ### Following is for building out an approval time factor correlation heatmap
 noTV <- approvalsQuestCoded[is.na(approvalsQuestCoded$TV.AT),]
-corout <- cor(noTV[,7:32], use = "pairwise.complete")
-corout <- melt(data = corout, varnames = c("x", "y"), value.name = "Correlations")
-#now order the result for plotting
-colcorsOrdered <- corout[order(corout$Correlations), ]
-
-#now plot as a heat map
-library(scales)
-ggplot(colcorsOrdered) +
-    aes(x=x, y=y) +
-    geom_tile(aes(fill=Correlations)) +
-    scale_fill_gradient2(low=muted("red"), mid="white", high="steelblue",
-                         guide=guide_colorbar(ticks = FALSE, barheight = 12),
-                         limits=c(-1,1)) +
-    theme_minimal() +
-    labs(x=NULL, y=NULL) +
-    ggtitle("Correlation Heat Map of Approval Times")
-
 ### Following is for building out an approval time factor correlation heatmap with QuestionsCoded
 corout <- cor(approvalsQuestCoded[,7:33], use = "pairwise.complete", method = "spearman")
 corout <- melt(data = corout, varnames = c("x", "y"), value.name = "Correlations")
+
 #now order the result for plotting
 colcorsOrdered <- corout[order(corout$Correlations), ]
 
 #now plot as a heat map
-library(scales)
 ggplot(colcorsOrdered) +
     aes(x=x, y=y) +
     geom_tile(aes(fill=Correlations)) +
