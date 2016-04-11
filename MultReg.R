60804b85c9b7bf1c682b97b8c9b68cb4e9371d97
theresamay <theresa.may.001@gmail.com>
2016-04-10 20:44:32 -0700

    Added Correlation matrix and heat map. Recoded categorical to numberical binary values for correlations.

--- MultReg.R
+++ MultReg.R
@@ -3,6 +3,8 @@
 # Also create Box Plot Series on codes
 ########################################################
 library(ggplot2)
+library(reshape2)
+library(dplyr)
 
 # Load in the data
 
@@ -61,11 +63,70 @@ ggplot(noTVCoded) +
                       values = c("darkslategray", "lightyellow1")) +
     coord_flip()
     
-    
 
-# Correlation Matrix - Heat Map
+#################################################
+### Working with correlation matrix
+#################################################
+
+# Original attempt uses a subset of the total dataframe, removing the columns that
+# have no place in the final correlation matrix.
+# Uses pairwise complete because of sparse nature of combinations,
+# and spearman for same reason.
+
+# Change y/n to 1/0 integers to prepare for correlation matrix
+
+CodesOnly <- noTVCoded[,52:59]
+
+to_nums <- function(value){
+    if (is.na(value)){
+        return(value)
+    } else if (value == "y"){
+        return("1")
+    } else if (value == "n")  {
+        return("0")
+    }
+}
+
+CodesOnly$Modification <- unlist(lapply(CodesOnly$Modification, function(s) to_nums(s)))
+CodesOnly$Modification <- unlist(lapply(CodesOnly$Modification, as.integer))
+
+CodesOnly$Complex <- unlist(lapply(CodesOnly$Complex, function(s) to_nums(s)))
+CodesOnly$Complex <- unlist(lapply(CodesOnly$Complex, as.integer))
+
+CodesOnly$Population <- unlist(lapply(CodesOnly$Population, function(s) to_nums(s)))
+CodesOnly$Population <- unlist(lapply(CodesOnly$Population, as.integer))
+
+CodesOnly$Security <- unlist(lapply(CodesOnly$Security, function(s) to_nums(s)))
+CodesOnly$Security <- unlist(lapply(CodesOnly$Security, as.integer))
+
+CodesOnly$ITS <- unlist(lapply(CodesOnly$ITS, function(s) to_nums(s)))
+CodesOnly$ITS <- unlist(lapply(CodesOnly$ITS, as.integer))
+
+CodesOnly$GenReq <- unlist(lapply(CodesOnly$GenReq, function(s) to_nums(s)))
+CodesOnly$GenReq <- unlist(lapply(CodesOnly$GenReq, as.integer))
+
+# Calculate the correlation matrix, then melt data and pass to appropriate correlogram
+corout <- cor(CodesOnly, use = "pairwise.complete", method = "spearman")
+corout <- melt(data = corout, varnames = c("x", "y"), value.name = "Correlations")
+
+#now order the result for plotting
+colcorsOrdered <- corout[order(corout$Correlations), ]
+
+#now plot as a heat map
+ggplot(colcorsOrdered) +
+    aes(x=x, y=y) +
+    geom_tile(aes(fill=Correlations)) +
+    scale_fill_gradient2(low=muted("red"), mid="white", high="steelblue",
+                         guide=guide_colorbar(ticks = FALSE, barheight = 12),
+                         limits=c(-1,1)) +
+    theme_minimal() +
+    labs(x=NULL, y=NULL) +
+    ggtitle("Correlation Heat Map of Approval Times\n Requests by Type")
+
+##########################################################
+### End correlation plot
+##########################################################
 
 
 # Multiple Regression Model
-
 
